fbb4d3c4c31a8160c2357a3a332390b2560e9b6d
zong006 <ongzhicong@gmail.com>
2025-03-26 02:46:39 +0800

    correct file path

--- final_backend/src/main/java/vttp/final_backend/config/FirebaseConfig.java
+++ final_backend/src/main/java/vttp/final_backend/config/FirebaseConfig.java
@@ -2,6 +2,7 @@ package vttp.final_backend.config;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
 
@@ -16,8 +17,9 @@ public class FirebaseConfig {
     
     @PostConstruct
     public void initializeSDK() throws IOException{
+        InputStream is = getClass().getClassLoader().getResourceAsStream("vttp-final-proj-firebase-adminsdk-fbsvc-6c1004315d.json");
         FileInputStream fis = new FileInputStream("./src/main/resources/vttp-final-proj-firebase-adminsdk-fbsvc-6c1004315d.json");
-        FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(fis)).build();
+        FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(is)).build();
         FirebaseApp.initializeApp(options);
     }
 
